a53893aa3e9634e42c90001b6b3cc0e7bba0d2c4
Jongho Kim <scurry2119@gmail.com>
2021-02-18 15:07:49 +0900

    Modulize start/end handler

--- parse_xml.c
+++ parse_xml.c
@@ -1,31 +1,14 @@
-/* 
-   A simple test program to parse XML documents with expat
-   <http://expat.sourceforge.net/>. It just displays the element
-   names.
-
-   On Debian, compile with:
-
-   (X) gcc -Wall -o expat-test -lexpat expat-test.c  
-   (O) gcc parse_expat.c -lexpat
-
-   Inspired from <http://www.xml.com/pub/a/1999/09/expat/index.html> 
-*/
-
-#include <expat.h>
 #include <stdio.h>
-#include <string.h>
+#include "parse_xml.h"
 
 /* Keep track of the current level in the XML tree */
-int             Depth;
+static int         depth = 0;
 
-#define MAXCHARS 1000000
 
-void
-start(void *data, const char *el, const char **attr)
-{
+void start(void *data, const char *el, const char **attr) {
     int             i;
 
-    for (i = 0; i < Depth; i++)
+    for (i = 0; i < depth; i++)
         printf("  ");
 
     printf("%s", el);
@@ -35,52 +18,9 @@ start(void *data, const char *el, const char **attr)
     }
 
     printf("\n");
-    Depth++;
+    depth++;
 }               /* End of start handler */
 
-void
-end(void *data, const char *el)
-{
-    Depth--;
+void end(void *data, const char *el) {
+    depth--;
 }               /* End of end handler */
-
-int
-main(int argc, char **argv)
-{
-
-    char           *filename;
-    FILE           *f;
-    size_t          size;
-    char           *xmltext;
-    XML_Parser      parser;
-
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s filename\n", argv[0]);
-        return (1);
-    }
-    filename = argv[1];
-    parser = XML_ParserCreate(NULL);
-    if (parser == NULL) {
-        fprintf(stderr, "Parser not created\n");
-        return (1);
-    }
-    /* Tell expat to use functions start() and end() each times it encounters
-     * the start or end of an element. */
-    XML_SetElementHandler(parser, start, end);
-    f = fopen(filename, "r");
-    xmltext = malloc(MAXCHARS);
-    /* Slurp the XML file in the buffer xmltext */
-    size = fread(xmltext, sizeof(char), MAXCHARS, f);
-    if (XML_Parse(parser, xmltext, strlen(xmltext), XML_TRUE) ==
-        XML_STATUS_ERROR) {
-        fprintf(stderr,
-            "Cannot parse %s, file may be too large or not well-formed XML\n",
-            filename);
-        return (1);
-    }
-    fclose(f);
-    XML_ParserFree(parser);
-    fprintf(stdout, "Successfully parsed %li characters in file %s\n", size,
-        filename);
-    return (0);
-}
---
+++ parse_xml.h
@@ -0,0 +1,7 @@
+#ifndef _PARSE_XML_H
+#define _PARSE_XML_H
+
+void start(void *data, const char *el, const char **attr);
+void end(void *data, const char *el);
+
+#endif   //_PARSE_XML_H
