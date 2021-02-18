39d600d9ef4a3e46eacf096ee611f09d46a77032
Jongho Kim <scurry2119@gmail.com>
2021-02-18 17:20:52 +0900

    Hide start() and end()

--- parse_xml.c
+++ parse_xml.c
@@ -3,13 +3,48 @@
 #include <expat.h>
 #include "parse_xml.h"
 
-#define FOUR_GIBIBYTE 4294967296   //  4 GiB
-#define TIBERO6_XMLTYPE_MAXSIZE   FOUR_GIBIBYTE   // 4 GiB
-#define BUFFSIZE   FOUR_GIBIBYTE
+#define FOUR_GIBIBYTE             4294967296   //  4 GiB
+#define TIBERO6_XMLTYPE_MAXSIZE   FOUR_GIBIBYTE
+#define BUFFSIZE                  FOUR_GIBIBYTE
+
+//
+#define SITE_INFO   "siteinfo"
+#define PAGE        "page"
 
 
 /* Keep track of the current level in the XML tree */
 static int         depth = 0;
+static size_t      count = 0;
+
+
+void start(void *data, const char *el, const char **attr) {
+  int             i;
+
+  for (i = 0; i < depth; i++)
+    printf("  ");
+
+  //if(!strcmp(PAGE, el)) {
+    // TODO: PUSH
+  //}
+
+  printf("%s", el);
+
+  for (i = 0; attr[i]; i += 2) {
+    printf(" %s='%s'", attr[i], attr[i + 1]);
+  }
+
+  printf("\n");
+  depth++;
+}               /* End of start handler */
+
+
+void end(void *data, const char *el) {
+  depth--;
+
+  //if(!strcmp(PAGE, el)) {
+    // TODO: POP
+  //}
+}               /* End of end handler */
 
 
 size_t parse_xml(const char *filename) {
@@ -49,23 +84,3 @@ size_t parse_xml(const char *filename) {
 
   return size;
 }
-
-void start(void *data, const char *el, const char **attr) {
-    int             i;
-
-    for (i = 0; i < depth; i++)
-        printf("  ");
-
-    printf("%s", el);
-
-    for (i = 0; attr[i]; i += 2) {
-        printf(" %s='%s'", attr[i], attr[i + 1]);
-    }
-
-    printf("\n");
-    depth++;
-}               /* End of start handler */
-
-void end(void *data, const char *el) {
-    depth--;
-}               /* End of end handler */
--- parse_xml.h
+++ parse_xml.h
@@ -2,7 +2,5 @@
 #define _PARSE_XML_H
 
 size_t parse_xml(const char *filename);
-void start(void *data, const char *el, const char **attr);
-void end(void *data, const char *el);
 
 #endif   //_PARSE_XML_H
